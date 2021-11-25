839ef4277bcce80456f14054514f405f39f5faff
Meinhard Wolfgang Kissich <m.kissich@student.tugraz.at>
2021-11-25 17:48:26 +0100

    Added initial structure

---
+++ scripts/source.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+source /tools/Xilinx/Vivado/2021.1/settings64.sh
---
+++ tb/tc_pkg.sv
@@ -0,0 +1,12 @@
+/*
+ * file: tc_pkg.sv
+ *
+ */
+
+package tb_pkg;
+  
+  const time CLK_PERIOD = 20ns;
+  
+
+
+endpackage
