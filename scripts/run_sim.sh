35bd9fc4b0312d7c07173ccc087d34f34a9ba13a
Meinhard Wolfgang Kissich <m.kissich@student.tugraz.at>
2021-11-29 17:51:05 +0100

    - Added some updates

---
+++ scripts/run_sim.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+source ../scripts/source.sh
+cd ../sim
+vivado -mode batch -source ../scripts/run_sim.tcl
--- sim/run_sim.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-source ../scripts/source.sh
-vivado -mode batch -source run_sim.tcl
