33c8cbc4d4957ddf04f7b0762ace1ea722c9c987
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-13 17:27:08 +0100

    Add documentation for indec interface

--- src/indec.sv
+++ src/indec.sv
@@ -7,36 +7,37 @@
 `default_nettype wire
 `timescale 1ns/1ps
 module indec (
-              // General
-              input  logic          clk_i,
-              input  logic          rst_in,
-              // Input
-              input  logic          stb_i,
-              input  logic [ 7:0]   opc_i,
-              // Output
-              output logic          sft_rst_o,
-              output logic          arm_o,
-              output logic          id_o,
-              output logic          set_mask_o,
-              output logic          set_val_o,
-              output logic          set_cfg_o,
-              output logic          set_div_o,
-              output logic          set_cnt_o,
-              output logic          set_flgs_o,
-              output logic [ 1:0]   stg_o,
-              // TODO: introduce reg if shortens critical path
-              output logic          stb_o,
-              // Flow Control
-              output logic          xstb_o,
-              output logic          xon_o,
-              output logic          xoff_o,
-              // OLS extension
-              output logic          rd_meta_o,
-              output logic          fin_now_o,
-              output logic          rd_inp_o,
-              output logic          arm_adv_o,
-              output logic          set_adv_cfg_o,
-              output logic          set_adv_dat_o);
+  // General
+  input  logic          clk_i,          //! system clock
+  input  logic          rst_in,         //! system reset, low active
+  // Input
+  input  logic          stb_i,          //! flag for valid op-code input
+  input  logic [ 7:0]   opc_i,          //! command op-code
+  // Output
+  output logic          sft_rst_o,      //! perform soft reset
+  output logic          arm_o,          //! arm the trigger
+  output logic          id_o,           //! request for device identification
+  output logic          set_mask_o,     //! set mask for trigger
+  output logic          set_val_o,      //! set trigger values
+  output logic          set_cfg_o,      //! configure trigger stage
+  output logic          set_div_o,      //! set frequency divider
+  output logic          set_cnt_o,      //! set the amount of samples to return
+  output logic          set_flgs_o,     //! configure flags
+  output logic [ 1:0]   stg_o,          //! stage
+  // TODO: introduce reg if shortens critical path
+  output logic          stb_o,          //! flag took sample
+  // Flow Control
+  output logic          xstb_o,         //! 
+  output logic          xon_o,          //! put transmitter out of pause mode
+  output logic          xoff_o,         //! put transmitter in pause mode
+  // OLS extension
+  output logic          rd_meta_o,      //! 
+  output logic          fin_now_o,      //! 
+  output logic          rd_inp_o,       //! 
+  output logic          arm_adv_o,      //! 
+  output logic          set_adv_cfg_o,  //! 
+  output logic          set_adv_dat_o   //! 
+);
 
   import logIP_pkg::*;
   
