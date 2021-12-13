7c2c6ba7263e67c21cd09c9a938597604a3b9643
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-13 08:16:38 +0100

    Fix source script for different vivado versions
    The source.sh script now looks for vivado installations at the default
    path and uses the latest installed version. If the procedure fails, error
    messages are printed.

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,3 +1,29 @@
 #! /bin/bash
 
-source /tools/Xilinx/Vivado/2021.1/settings64.sh
+vivadoPath=/tools/Xilinx/Vivado
+fileName=settings64.sh
+
+if [ ! -d $vivadoPath ] 
+then
+    echo "[ERROR] No Vivado installation found. If Vivado is installed in a different directory then default, correct the path in scripts/source.sh."
+    exit
+fi
+
+versionPaths=($(find $vivadoPath/ -maxdepth 1 -name '20*.*' -type d | sort))
+versionPath=${versionPaths[-1]}
+
+if [ -z $versionPath ]
+then
+    echo "[ERROR] No valid Vivado version found."
+    exit
+fi
+
+fullPath=$versionPath/$fileName
+
+if [ ! -f $fullPath ]
+then
+    echo "[ERROR] No valid Vivado installation found."
+    exit
+fi
+
+source $fullPath
