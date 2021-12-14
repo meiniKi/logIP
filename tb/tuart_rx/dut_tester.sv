be2aed2ce5d15e5e4ecf5e6ffb40185c9219700d
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-14 07:32:46 +0100

    Add test case for uart receiver
    The test case includes a test for a short command, a long command
    and concurrent short commands.

--- tb/tuart_rx/dut_tester.sv
+++ tb/tuart_rx/dut_tester.sv
@@ -5,6 +5,8 @@
 `include "declarations.svh"
 `default_nettype wire
 `timescale 1ns/1ps
+`define BUS_BIT_DELAY       #(CLK_PERIOD_HALF*DS)
+
 program uart_rx_tester ( dut_if.tb duv_if, input clk_i, input score_mbox_t mbx);
   import tb_pkg::*;
 
@@ -17,22 +19,66 @@ program uart_rx_tester ( dut_if.tb duv_if, input clk_i, input score_mbox_t mbx);
     $display("----- Started ------");
     $display("-- %d cycles per bit", DS);
 
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
+
+    // ##### Test a short command #####
+    // Start bit
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;
+
+    // For short command first bit has to be zero
+    repeat(7) `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;
+
+    // Stop bit
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
+
+    $write("[INFO] Waiting for posedge on strobe... ");
+    @(posedge duv_if.cb.stb_o);
+    $display("DONE");
+    `SCORE_ASSERT(duv_if.cb.data_o == 'h7F000000_00);
+
+
+    // ##### Test a long command #####
     repeat (5) begin
-      #(CLK_PERIOD_HALF*DS) duv_if.cb.rx_async_i <= 1;
-      #(CLK_PERIOD_HALF*DS) duv_if.cb.rx_async_i <= 0;
-
-      repeat(8) #(CLK_PERIOD_HALF*DS) duv_if.cb.rx_async_i <= 1;
-
-      // Test asserts
-      `SCORE_ASSERT(1)
-      `SCORE_ASSERT(0)
-      `SCORE_DONE
+      `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;
+    
+      repeat(8) `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
       
-      #(CLK_PERIOD_HALF*DS)  duv_if.cb.rx_async_i <= 0;
-      #(CLK_PERIOD_HALF*DS)  duv_if.cb.rx_async_i <= 1;
-
-      // TODO
+      `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
     end
+    
+    $write("[INFO] Waiting for posedge on strobe... ");
+    @(posedge duv_if.cb.stb_o);
+    $display("DONE");
+    `SCORE_ASSERT(duv_if.cb.data_o == 'hFFFFFFFF_FF);
+
+
+    // ##### Test concurrent short commands #####
+    
+    // First short command
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;             // Start bit
+    repeat(7) `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;             // Stop bit
+
+    $write("[INFO] Waiting for posedge on strobe... ");
+    @(posedge duv_if.cb.stb_o);
+    $display("DONE");
+    `SCORE_ASSERT(duv_if.cb.data_o == 'h7F000000_00);
+
+    // Second short command
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;             // Start bit
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;
+    repeat(7) `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 0;
+    `BUS_BIT_DELAY duv_if.cb.rx_async_i <= 1;             // Stop bit
+
+    $write("[INFO] Waiting for posedge on strobe... ");
+    @(posedge duv_if.cb.stb_o);
+    $display("DONE");
+    `SCORE_ASSERT(duv_if.cb.data_o == 'h01000000_00);
+
+    `SCORE_DONE
+
     $display("----- Done ------");
     #100000 $finish;
   end
