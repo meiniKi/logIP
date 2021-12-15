651923de29cb3f6cfce5a9a3558c767920ebca49
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-15 07:49:41 +0100

    Add test case skeleton for stage

---
+++ tb/stage/dut_tester.sv
@@ -0,0 +1,24 @@
+/*
+ * file: dut_tester.sv
+ * 
+ */
+`include "declarations.svh"
+`default_nettype wire
+`timescale 1ns/1ps
+`define CLK_DELAY #(CLK_PERIOD_HALF*2)
+
+program stage_tester ( dut_if.tb duv_if, input clk_i, input score_mbox_t mbx);
+  import tb_pkg::*;
+
+  initial begin
+    $display("----- Started ------");
+
+    // TODO
+
+    `SCORE_DONE
+      
+    $display("----- Done ------");
+    #100000 $finish;
+  end
+
+endprogram
---
+++ tb/stage/stage_tb.sv
@@ -0,0 +1,52 @@
+/*
+ * file: stage_tb.sv
+ * usage: Testbench for stage.sv
+ * 
+ */
+
+`include "declarations.svh"
+`default_nettype wire
+`timescale 1ns/1ps
+
+module stage_tb;
+  import tb_pkg::*;
+
+  logic clk_i  = 0;
+  logic rst_in = 0;
+
+  Scoreboard      i_scoreboard;
+  score_mbox_t    mbx;       
+
+  initial begin
+    // Dump
+    $dumpfile("stage_tb.vcd");
+    $dumpvars(5, duv_wrapper);
+
+    // Reset            
+    #(10*CLK_PERIOD_HALF) rst_in = 0;
+    #(2*CLK_PERIOD_HALF)  rst_in = 1;
+  end
+
+  always begin : clock_gen
+    #(CLK_PERIOD_HALF) clk_i = 1;
+    #(CLK_PERIOD_HALF) clk_i = 0;
+  end
+
+  dut_if duv_if (clk_i, rst_in);
+  dut_wrapper duv_wrapper (duv_if.duv);
+  stage_tester duv_tester(duv_if.tb, clk_i, mbx);
+
+  initial begin
+    mbx = new();
+    i_scoreboard = new (mbx);
+
+    fork
+      i_scoreboard.run();
+      // append
+    join
+  end
+
+endmodule
+
+
+
