fb4a773843eda8d0946609dadb7900fe1d109fd8
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-15 08:30:43 +0100

    Add simple test case for stage
    The test case checks if stage triggers correctly on a match
    depending on its armed state on a single input channel.

--- tb/stage/dut_tester.sv
+++ tb/stage/dut_tester.sv
@@ -13,7 +13,36 @@ program stage_tester ( dut_if.tb duv_if, input clk_i, input score_mbox_t mbx);
   initial begin
     $display("----- Started ------");
 
-    // TODO
+    duv_if.cb.smpls_i     <= 'h00000000;
+    duv_if.cb.cmd_i       <= 'h00000001;
+    duv_if.cb.arm_i       <= 'b0;
+    duv_if.cb.set_mask_i  <= 'b0;
+    duv_if.cb.set_val_i   <= 'b0;
+    duv_if.cb.set_cfg_i   <= 'b0;
+    duv_if.cb.stb_i       <= 'b0;
+
+    #(65)
+
+    // ##### Test arm and trigger #####
+    // Configuration
+    `CLK_DELAY duv_if.cb.set_mask_i <= 'b1;
+    `CLK_DELAY duv_if.cb.set_mask_i <= 'b0;
+    duv_if.cb.cmd_i <= 'hFFFFFFFF;
+    `CLK_DELAY duv_if.cb.set_val_i <= 'b1;
+    `CLK_DELAY duv_if.cb.set_val_i <= 'b0;
+    duv_if.cb.cmd_i <= 'h00000000;
+
+    // Match when not armed
+    duv_if.cb.smpls_i <= 'h00000001;
+    `CLK_DELAY duv_if.cb.stb_i <= 'b1;
+    `CLK_DELAY `SCORE_ASSERT(duv_if.cb.run_o == 'b0);
+    duv_if.cb.stb_i <= 'b0;
+    // Match when armed
+    `CLK_DELAY duv_if.cb.arm_i <= 'b1;
+    `CLK_DELAY duv_if.cb.arm_i <= 'b0;
+    `CLK_DELAY duv_if.cb.stb_i <= 'b1;
+    `CLK_DELAY `SCORE_ASSERT(duv_if.cb.run_o == 'b1);
+    duv_if.cb.stb_i <= 'b0;
 
     `SCORE_DONE
       
