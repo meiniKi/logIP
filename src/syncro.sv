0131b6bd37ca9614623bf6b14108eb65282fa14d
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2021-12-17 07:43:02 +0100

    New version of syncro

--- src/syncro.sv
+++ src/syncro.sv
@@ -4,23 +4,26 @@
  */
 
 `default_nettype wire
-module syncro #(parameter WIDTH = 1,
-                parameter INIT_VAL = 1) (
-                                    input  logic              clk_i,
-                                    input  logic              rst_in,
-                                    input  logic [WIDTH-1:0]  async_i,
-                                    output logic [WIDTH-1:0]  sync_o);
+module syncro #(
+  parameter WIDTH = 32,                 //! signal width
+  parameter INIT_VAL = 'b0              //! initial value
+) (
+  input  logic              clk_i,      //! system clock 
+  input  logic              rst_in,     //! system reset, low active
+  input  logic [WIDTH-1:0]  async_i,    //! asynchronous input
+  output logic [WIDTH-1:0]  sync_o      //! synchronized output
+);
 
-logic [WIDTH-1:0] inter;
-                                  
-always_ff @(posedge clk_i) begin
-  if (rst_in == 'b0) begin
-    inter   <= INIT_VAL;
-    sync_o  <= INIT_VAL;
-  end else begin
-    inter   <= async_i;
-    sync_o  <= inter;
+  logic [WIDTH-1:0] inter;
+
+  assign sync_o = inter;
+
+  always_ff @(posedge clk_i) begin
+    if (rst_in == 'b0) begin
+      inter   <= INIT_VAL;
+    end else begin
+      inter   <= async_i;
+    end
   end
-end
 
 endmodule
